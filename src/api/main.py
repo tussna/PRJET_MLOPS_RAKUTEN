047a11f5c22a3c49c29caf52a400ae6bb132d43a
tussna <129739904+tussna@users.noreply.github.com>
2023-06-05 18:43:02 +0000

    Complete main file

--- src/api/main.py
+++ src/api/main.py
@@ -1,1 +1,9 @@
-print("Hello test")
+import uvicorn
+
+def main():
+    uvicorn.run("routers.api:app", port = 8000, reload = True)
+    #uvicorn.run("api:app", port = 8000, host = '0.0.0.0', reload = True)
+    #uvicorn.run("api:app", port = 8000, host = '0.0.0.0', debug = True, reload = True)
+
+if __name__ == "__main__":
+    main()
