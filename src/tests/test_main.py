0e440bdd47d08676810cd32fda1a4725b0d24a16
tussna <129739904+tussna@users.noreply.github.com>
2023-06-05 19:12:31 +0000

    Create a tests

---
+++ src/tests/test_main.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+import json
+
+from app.main import app
+
+client = TestClient(app)
+
+def test_home():
+    response = client.get("/home", headers = {"content-type": "html; charset=utf-8"})
+    assert response.status_code == 200
+    assert b"Welcome to FastAPI Starter" in response.content
+    response = client.get("app//static/css/style.css")
+    assert response.status_code == 200
+
+def test_page_about():
+    response = client.get("/about",
+                          headers={"content-type": "html; charset=utf-8"})
+    assert response.status_code == 200
+    assert b"About" in response.content
